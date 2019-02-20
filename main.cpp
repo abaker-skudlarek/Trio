433d5fde89c1e2794a8097e01b2afb698c907057
abaker-skudlarek@mail.csuchico.edu <abaker-skudlarek@mail.csuchico.edu>
2019-02-20 11:21:32 -0800

    Added documentation and fixed sorting (issue #1)

--- main.cpp
+++ main.cpp
@@ -3,9 +3,17 @@ using std::cout;
 using std::cin;
 using std::endl;
 
-void sortDescending(int,int,int);
+void sortDescending(int&,int&,int&);
 void swap(int&,int&);
 
+/*
+ * main()
+ * 
+ * main driver function, code starts running here. takes input and 
+ *  calls sortDescending function then prints final output
+ * 
+ * @return 0 at the end of main to indicate a successful run
+ */
 int main()
 {
   int numA, numB, numC;
@@ -19,7 +27,17 @@ int main()
   return 0;
 }
 
-void sortDescending(int first, int second, int third)
+/**
+ * sortDescending(int&, int&, int&)
+ *
+ * compares all three numbers and determines if they need to be swapped
+ *  if they do, it calls the swap() function
+ * 
+ * @param &first  is first number in sequence
+ * @param &second is second number in sequence
+ * @param &third  is third number in sequence
+ */
+void sortDescending(int &first, int &second, int &third)
 {
   if( first < third )
   {
@@ -35,6 +53,15 @@ void sortDescending(int first, int second, int third)
   }
 }
 
+/**
+ * swap(int&, int&)
+ * 
+ * swaps two numbers by making the first one equal to the second and the 
+ *  second equal to the first
+ * 
+ * @param &first  First number to swap
+ * @param &second Second number to swap
+ */
 void swap(int &first, int &second)
 {
   int temp = first;
